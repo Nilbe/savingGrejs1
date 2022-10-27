3d7bce1809d851c7b71ca3e03f357158cdd9bb47
Nilbe <90683548+Nilbe@users.noreply.github.com>
2022-10-27 10:45:24 +0200

    commit1

---
+++ savingGrejs1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+survivor player = new();
+
+
+player.strength = 6;
+player.durability = 4;
+
+Console.WriteLine("Hello survivior, what is your name?");
+player.name = Console.ReadLine();
+
+
+string save = JsonSerializer.Serialize<survivor>(player);
+
+File.WriteAllText("save.txt", save);
+
+Console.WriteLine(save);
+
+Console.ReadLine();
---
+++ savingGrejs1/survivor.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+public class survivor
+{
+        public int strength {get;set;}
+    public int durability {get;set;}
+    public string name {get;set;}
+}
